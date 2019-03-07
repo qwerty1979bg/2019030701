57f8b947173c1516b9428d57efd7914a19e268cb
qwerty1979bg <42143379+qwerty1979bg@users.noreply.github.com>
2019-03-07 17:17:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "null_resource" "test" {}
 output "test" {
   value = "${null_resource.test.id}"
 }
+
+resource "null_resource" "foo" {
+  provisioner "local-exec" {
+    command = "cat /terraform/*.tf ; ls -la /terraform"
+  }
+}
